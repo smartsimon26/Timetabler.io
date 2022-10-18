8b0086938beace261c095145d538d72fc650b236
Smart Simon <ndungusimonm@gmail.com>
2022-10-18 14:42:14 +0300

    Creation of tables in index

---
+++ script.js
@@ -0,0 +1,41 @@
+let curriculum = [
+    SIT400 = {
+        lecturer:"Mr Mwai",
+        Day: "Mon",
+        Timeshift:"noon",
+        venue:"GPL2",
+    },
+    SIT401 = {
+        lecturer:"Mr Mwai",
+        Day: "Mon",
+        Timeshift:"noon",
+        venue:"GPL2",
+    },
+    SIT402 = {
+        lecturer:"Mr Mwai",
+        Day: "Mon",
+        Timeshift:"noon",
+        venue:"GPL2",
+    },
+    SIT403 = {
+        lecturer:"Mr Mwai",
+        Day: "Mon",
+        Timeshift:"noon",
+        venue:"GPL2",
+    },
+    SIT404 = {
+        lecturer:"Mr Mwai",
+        Day: "Mon",
+        Timeshift:"noon",
+        venue:"GPL2",
+    },
+    SIT405 = {
+        lecturer:"Mr Mwai",
+        Day: "Mon",
+        Timeshift:"noon",
+        venue:"GPL2",
+    }
+];
+const schedule= document.getElementById('tt');
+
+schedule.innerHTML='<table> <tr><td>hello world</td> </tr> </table>';
