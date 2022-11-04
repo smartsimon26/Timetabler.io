ff8e72abd0675725d6f8ce808ea2fbda17ec98c4
Smart Simon <ndungusimonm@gmail.com>
2022-11-05 02:19:02 +0300

    Adde tabs to input.html

--- JsForComponents.js
+++ JsForComponents.js
@@ -4,3 +4,13 @@ searchButton.addEventListener('click', () => {
   const inputValue = searchInput.value;
   alert(inputValue);
 });
+
+const triggerTabList = document.querySelectorAll('#myTab button')
+triggerTabList.forEach(triggerEl => {
+  const tabTrigger = new bootstrap.Tab(triggerEl)
+
+  triggerEl.addEventListener('click', event => {
+    event.preventDefault()
+    tabTrigger.show()
+  })
+})
