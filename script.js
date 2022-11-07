10f2d98903c02989e5685d4ab810e4490609d7b8
Smart Simon <ndungusimonm@gmail.com>
2022-11-07 10:59:59 +0300

    changed btstrp links to repair tabs

--- script.js
+++ script.js
@@ -67,7 +67,7 @@ const venues = ['1PL1', '2PL2', '3PL3', '1PL4', '2PL6'];
 const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'];
 
 let thead = document.getElementById('thead');
-thead.innerHTML = `<tr><td>${tabletitles[0]}</td><td>${tabletitles[1]}</td><td>${tabletitles[2]}</td><td>${tabletitles[3]}</td><td>${tabletitles[4]}</td></tr>`
+thead.innerHTML=`<tr><td>${tabletitles[0]}</td><td>${tabletitles[1]}</td><td>${tabletitles[2]}</td><td>${tabletitles[3]}</td><td>${tabletitles[4]}</td></tr>`
 
 
 showdata(curriculum)
