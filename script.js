f0c5126d3a4f336c8d3c5f3f4123d281a2d2b3c0
Peter Ndungu <74706199+Peter-Wanjama@users.noreply.github.com>
2022-11-08 00:00:51 +0300

    commit

--- script.js
+++ script.js
@@ -70,10 +70,10 @@ let thead = document.getElementById('thead');
 thead.innerHTML = `<tr><td>${tabletitles[0]}</td><td>${tabletitles[1]}</td><td>${tabletitles[2]}</td><td>${tabletitles[3]}</td><td>${tabletitles[4]}</td></tr>`
 
 
-showdata(curriculum)
+//showdata(curriculum)
 
 
-function showdata(data) {
+/* function showdata(data) {
     var tbody = document.getElementById('tbody');
 
     for (let i = 0; i < data.length; i++) {
@@ -96,44 +96,126 @@ function showdata(data) {
                         </tr>`
         tbody.innerHTML += lecture
     }
-}
-function addTimeshift() {
-    $("#timeshift_form").submit(function (e) {
-        e.preventDefault();
-    });
-    document.getElementById("timeshift_table").hidden = false;
-    var x = $("#session_time").val();
-    if (x === "") {
-        return;
-    }
-    console.log("You entered " + x)
-    // Save to database
-    var n_sessions = $("#timeshift_table tbody tr").length + 1;
-    // Ajax
-    var xmlhttp;
-    if (window.XMLHttpRequest) {
-        // code for IE7+, Firefox, Chrome, Opera, Safari
-        xmlhttp = new XMLHttpRequest();
-    } else {
-        // code for IE6, IE5
-        xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-    }
-    var message = "";
-    const servercodes = ["SUCCESS", "FAILED:"];
-    xmlhttp.onreadystatechange = function () {
-        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-            var response = xmlhttp.responseText;
-            // code when server responds
-            message = response.substring(0, 7);
-            console.log(response);
-            if (message == servercodes[0]) {// If no error occured
-                // Add the value to table
-                $('#timeshift_table > tbody:last-child').append('<tr><td>' + n_sessions + '</td><td>' + x + '</td></tr>');
-                $("#session_time").val("");
-            }
+} */
+function addSetting(setting) {
+    if (setting == "timeshifts") {
+        console.log("timeshifts");
+        $("#timeshift_form").submit(function (e) {
+            e.preventDefault();
+        });
+        document.getElementById("timeshift_table").hidden = false;
+        var x = $("#session_time").val();
+        if (x === "") {
+            return;
         }
-    };
-    xmlhttp.open("GET", "saveSettings.php?timeshift=" + x, true);
-    xmlhttp.send();
+        console.log("You entered " + x)
+        // Save to database
+        var n_sessions = $("#timeshift_table tbody tr").length + 1;
+        // Ajax
+        var xmlhttp;
+        if (window.XMLHttpRequest) {
+            // code for IE7+, Firefox, Chrome, Opera, Safari
+            xmlhttp = new XMLHttpRequest();
+        } else {
+            // code for IE6, IE5
+            xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+        }
+        var message = "";
+        const servercodes = ["SUCCESS", "FAILED:"];
+        xmlhttp.onreadystatechange = function () {
+            if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+                var response = xmlhttp.responseText;
+                // code when server responds
+                message = response.substring(0, 7);
+                console.log(response);
+                if (message == servercodes[0]) {// If no error occured
+                    // Add the value to table
+                    $('#timeshift_table > tbody:last-child').append('<tr><td>' + n_sessions + '</td><td>' + x + '</td></tr>');
+                    $("#session_time").val("");
+                }
+            }
+        };
+        xmlhttp.open("GET", "saveSettings.php?timeshift=" + x, true);
+        xmlhttp.send();
+    } else if (setting == "venues") {
+        console.log("venues");
+        $("#venues_form").submit(function (e) {
+            e.preventDefault();
+        });
+        document.getElementById("venues_table").hidden = false;
+        var x = $("#session_venue").val();
+        if (x === "") {
+            return;
+        }
+        console.log("You entered " + x)
+        // Save to database
+        var n_sessions = $("#venues_table tbody tr").length + 1;
+        // Ajax
+        var xmlhttp;
+        if (window.XMLHttpRequest) {
+            // code for IE7+, Firefox, Chrome, Opera, Safari
+            xmlhttp = new XMLHttpRequest();
+        } else {
+            // code for IE6, IE5
+            xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+        }
+        var message = "";
+        const servercodes = ["SUCCESS", "FAILED:"];
+        xmlhttp.onreadystatechange = function () {
+            if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+                var response = xmlhttp.responseText;
+                // code when server responds
+                message = response.substring(0, 7);
+                console.log(response);
+                if (message == servercodes[0]) {// If no error occured
+                    // Add the value to table
+                    $('#venues_table > tbody:last-child').append('<tr><td>' + n_sessions + '</td><td>' + x + '</td></tr>');
+                    $("#session_venue").val("");
+                }
+            }
+        };
+        xmlhttp.open("GET", "saveSettings.php?venue=" + x, true);
+        xmlhttp.send();
+    } else if (setting == "days") {
+        console.log("days");
+        $("#days_form").submit(function (e) {
+            e.preventDefault();
+        });
+        document.getElementById("days_table").hidden = false;
+        var x = $("#session_day").val();
+        if (x === "") {
+            return;
+        }
+        console.log("You entered " + x)
+        // Save to database
+        var n_sessions = $("#days_table tbody tr").length + 1;
+        // Ajax
+        var xmlhttp;
+        if (window.XMLHttpRequest) {
+            // code for IE7+, Firefox, Chrome, Opera, Safari
+            xmlhttp = new XMLHttpRequest();
+        } else {
+            // code for IE6, IE5
+            xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+        }
+        var message = "";
+        const servercodes = ["SUCCESS", "FAILED:"];
+        xmlhttp.onreadystatechange = function () {
+            if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+                var response = xmlhttp.responseText;
+                // code when server responds
+                message = response.substring(0, 7);
+                console.log(response);
+                if (message == servercodes[0]) {// If no error occured
+                    // Add the value to table
+                    $('#days_table > tbody:last-child').append('<tr><td>' + n_sessions + '</td><td>' + x + '</td></tr>');
+                    $("#session_day").val("");
+                }
+            }
+        };
+        xmlhttp.open("GET", "saveSettings.php?day=" + x, true);
+        xmlhttp.send();
+    }
+
 
 }
