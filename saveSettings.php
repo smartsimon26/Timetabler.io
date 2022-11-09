163a2540a6aa36d3bd5e1b0ee6f72f7f3cba14f9
Peter Ndungu <74706199+Peter-Wanjama@users.noreply.github.com>
2022-11-09 16:38:14 +0300

    commit

--- saveSettings.php
+++ saveSettings.php
@@ -10,12 +10,12 @@ try {
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $conn->exec("CREATE DATABASE IF NOT EXISTS timetabler");
-    $conn->exec('USE timetabler');
+    $conn->exec("USE timetabler");
 } catch (PDOException $e) {
     echo "FAILED:" . $e->getMessage();
 }
-if (isset($_GET['timeshift'])) {
-    $timeshift = $_GET['timeshift'];
+if (isset($_GET["timeshift"])) {
+    $timeshift = $_GET["timeshift"];
     try {
         //$sql="CREATE TABLE IF NOT EXISTS timeshifts(id int not null auto_increment,timeshift varchar(20) not null unique, primary key(id)";
         $sql = "CREATE TABLE IF NOT EXISTS `timetabler`.`timeshifts` ( `id` INT NOT NULL AUTO_INCREMENT , `timeshift` VARCHAR(20) NOT NULL , 
@@ -30,8 +30,8 @@ if (isset($_GET['timeshift'])) {
     } catch (PDOException $e) {
         echo "FAILED:" . $e->getMessage();
     }
-} elseif (isset($_GET['venue'])) {
-    $venue = $_GET['venue'];
+} elseif (isset($_GET["venue"])) {
+    $venue = $_GET["venue"];
     try {
         $sql = "CREATE TABLE IF NOT EXISTS `timetabler`.`venues` ( `id` INT NOT NULL AUTO_INCREMENT , `venue` VARCHAR(20) NOT NULL , 
         PRIMARY KEY (`id`), UNIQUE (`venue`)) ENGINE = InnoDB;";
@@ -45,8 +45,8 @@ if (isset($_GET['timeshift'])) {
     } catch (PDOException $e) {
         echo "FAILED:" . $e->getMessage();
     }
-} elseif (isset($_GET['day'])) {
-    $day = $_GET['day'];
+} elseif (isset($_GET["day"])) {
+    $day = $_GET["day"];
     try {
         $sql = "CREATE TABLE IF NOT EXISTS `timetabler`.`days` ( `id` INT NOT NULL AUTO_INCREMENT , `day` VARCHAR(20) NOT NULL , 
         PRIMARY KEY (`id`), UNIQUE (`day`)) ENGINE = InnoDB;";
